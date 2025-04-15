cb27ac62626f49e3dbf4f8fe9865c5873d789d81
Daniel Huss <63384617+Daniel-N-Huss@users.noreply.github.com>
2025-04-15 16:53:38 -0600

    add loading spinner

--- app/page.js
+++ app/page.js
@@ -35,7 +35,7 @@ const LLMallard = () => {
 
       setChatHistory(prevHistory => [...prevHistory, { type: 'bot', content: randomResponse }]);
       setIsLoading(false);
-    }, 150000);
+    }, 15000);
   };
 
   return (
@@ -53,6 +53,10 @@ const LLMallard = () => {
                     {message.content}
                   </div>
               ))}
+
+              {isLoading && (
+                  <div className={`${styles.thinkingIconContainer}`}/>
+              )}
             </div>
           </div>
         </main>
@@ -73,18 +77,6 @@ const LLMallard = () => {
             </form>
           </div>
         </div>
-
-        {isLoading && (
-            <div className={`${styles.thinkingIconContainer} active`}>
-              <div className={styles.spinner}>
-                <div className="dot1"></div>
-                <div className="dot2"></div>
-                <div className="dot3"></div>
-                <div className="dot4"></div>
-                <div className="dot5"></div>
-              </div>
-            </div>
-        )}
       </div>
   );
 };
